0e9a269c647b7fec6baa611a00e5463abcd93f83
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-19 15:07:45 +0200

    done

--- lib/between_strict.rb
+++ lib/between_strict.rb
@@ -1,8 +1,8 @@
-def between(term1, term2, term3)
+def between_strict(term1, term2, term3)
     if term2 - term1 == term3 - term2
         return true
     end
     return false
 end
 
-puts between(1,2,3)
+puts between_strict(1,2,3)
---
+++ lib/min_of_two.rb
@@ -0,0 +1,6 @@
+def min_of_two(term1, term2)
+    if term1 < term2
+        return term1
+    end
+    return term2
+end
