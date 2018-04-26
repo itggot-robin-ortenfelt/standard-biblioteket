7f3d1532edd9c6535cc248551724f0783e5fa47a
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:30 +0200

    tomdoc

--- lib/is_negative.rb
+++ lib/is_negative.rb
@@ -1,3 +1,16 @@
+# Public: decides if an integer is negative
+#
+# term  - the integer which will be examined
+# 
+# Examples
+#
+#   is_negative(6)
+#   # => false
+#
+#   is_negative(-4)
+#   # => true
+#
+# Returns true/false depending on if the integer is positive or negative.
 def is_negative(term1)
     output = false
     if term1 < 0
