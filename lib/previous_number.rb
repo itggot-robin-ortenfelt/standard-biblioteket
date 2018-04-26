bbbf61bca5218150073bf887b7379fbdebd5ce9c
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:12 +0200

    tomdoc

--- lib/previous_number.rb
+++ lib/previous_number.rb
@@ -1,3 +1,13 @@
+# Public: prints the number previous to the input
+#
+# term1  - the origional number.
+# 
+# Examples
+#
+#   previous_number(4)
+#   # => '3'
+#
+# Returns the previous number.
 def previous_number(term1)
         return term1.to_i - 1
     end
