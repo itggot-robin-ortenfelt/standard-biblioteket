d7e3cb21c188ad5c737020bb02682f577d1297e6
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:54 +0200

    tomdoc

--- lib/absolute.rb
+++ lib/absolute.rb
@@ -1,6 +1,17 @@
+# Public: outputs the absolute value of an integer
+#
+# term  - the integer that will be examined
+# 
+# Examples
+#
+#   absolute(-5)
+#   # => 5
+#
+# Returns true/false depending on if the integer is the same amount from term2 as it is form negative term3
 def absolute(term)
     
         if term < 0
             term * -1
-        end       
+        end
+        return term       
 end
