ab1622b55846f8f56560622b579c5dfce3a42132
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:37 +0200

    tomdoc

--- lib/is_even.rb
+++ lib/is_even.rb
@@ -1,3 +1,16 @@
+# Public: decides if an integer is even
+#
+# term  - the integer which will be examined
+# 
+# Examples
+#
+#   is_even(7)
+#   # => false
+#
+#   is_even(6)
+#   # => true
+#
+# Returns true/false depending on if the integer is even or odd.
 def is_even(term)
     output = false
     if term % 2 == 0
