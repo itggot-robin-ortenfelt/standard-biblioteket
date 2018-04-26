c0e9af88e35a40caaff058ed1e01aa366f5f9235
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:01 +0200

    tomdoc

--- lib/next_number.rb
+++ lib/next_number.rb
@@ -1,3 +1,13 @@
+# Public: prints the number after the input
+#
+# term1  - the origional number.
+# 
+# Examples
+#
+#   next_number(4)
+#   # => '5'
+#
+# Returns the next number.
 def next_number(term1)
     answ = term1.to_i + 1
     return answ
