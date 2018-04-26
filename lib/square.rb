5d331e1d6bae499ffcf4bf427f59e459f4ab5350
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:32:57 +0200

    tomdoc

--- lib/square.rb
+++ lib/square.rb
@@ -1,3 +1,13 @@
+# Public: Duplicate a interger with itself
+#
+# term1  - The String to be squared.
+# 
+# Examples
+#
+#   square(4)
+#   # => '16'
+#
+# Returns the squared integer.
 def square(term1)
     return term1.to_i ** 2
 end
