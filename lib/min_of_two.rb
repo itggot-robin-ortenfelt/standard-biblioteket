3c7614f0f86e43e1c5b26db1ce398d68b8795c04
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:16 +0200

    tomdoc

--- lib/min_of_two.rb
+++ lib/min_of_two.rb
@@ -1,3 +1,14 @@
+# Public: decides the smallest of two integers
+#
+# term1  - on of the integers
+# term2  - the other integer
+# 
+# Examples
+#
+#   min_of_two(4, 5)
+#   # => '4'
+#
+# Returns the smallest of the two integers.
 def min_of_two(term1, term2)
     if term1 < term2
         return term1
