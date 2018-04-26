ce43261ec13ab3e00483e98028bea400a7d324d6
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:40 +0200

    tomdoc

--- lib/cube.rb
+++ lib/cube.rb
@@ -1,3 +1,13 @@
-def square(term1)
+# Public: calculates the cubed version of an integer
+#
+# term  - the integer which will be cubed
+# 
+# Examples
+#
+#   cube(2)
+#   # => 8
+#
+# Returns the cubed version of the original integer
+def cube(term1)
     return term1.to_i ** 3
 end
