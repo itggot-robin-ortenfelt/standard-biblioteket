4aa18079282a43dc5dcf63cd3b35e5024b6a9ffe
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2018-04-26 14:33:20 +0200

    tomdoc

--- lib/is_odd.rb
+++ lib/is_odd.rb
@@ -1,3 +1,16 @@
+# Public: decides if an integer is odd
+#
+# term  - the integer which will be examined
+# 
+# Examples
+#
+#   is_odd(6)
+#   # => false
+#
+#   is_odd(7)
+#   # => true
+#
+# Returns true/false depending on if the integer is odd or even.
 def is_odd(term)
     output = true
     if term % 2 == 0
